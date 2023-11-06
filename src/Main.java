406726e37d33530de8c7afc7b724f43ffd7d8432
ibrahim inanç <iibrahim.inanc@gmail.com>
2023-11-06 20:56:16 +0300

    For Döngüsü

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /* int i = 1 demek döngü 1 den başlıyor demek ,  i<10  koşul demek yani i 10 a gelene kadar kodu çalıştır , i+ = 2   i'yi 2 artır demek.
+
+
+         */
+
+        for (int i = 1; i < 10; i += 2) {
+            System.out.println(i);
+        }
+
+        System.out.println("Döngü Bitti");
+
+
+    }
+}
